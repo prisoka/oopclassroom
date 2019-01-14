0388a02b98a12c417216f6a8894fce619eaa8c2c
Priscilla Okawa <prisoka@gmail.com>
2019-01-14 15:45:57 -0800

    class computer OK

--- src/computer.js
+++ src/computer.js
@@ -1,5 +1,16 @@
 class Computer {
-  
+  constructor(name, operatingSystem, processor, memory, graphics){
+    this.name = name;
+    this.operatingSystem = operatingSystem;
+    this.processor = processor;
+    this.memory = memory;
+    this.graphics = graphics;
+    this.on = false;
+  }
+
+  power(){
+    this.on = !this.on
+  }
 }
 
 module.exports = Computer
--- src/desk.js
+++ src/desk.js
@@ -1,5 +1,12 @@
 class Desk {
-  
+  constructor(length, width, height, isWhiteboard, content){
+    this.length = length;
+    this.width = width;
+    this.height = height;
+    this.isWhiteboard = false;
+    this.content = "";
+  }
+
 }
 
 module.exports = Desk
