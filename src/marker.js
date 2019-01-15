56786ecf87b69fdfc3c52138508e3c09a80b9f65
Priscilla Okawa <prisoka@gmail.com>
2019-01-14 16:02:58 -0800

    class students OK

--- src/marker.js
+++ src/marker.js
@@ -1,5 +1,13 @@
 class Marker {
-  
+  constructor(size, color, remainingInk){
+    this.size = size;
+    this.color = color;
+    this.remainingInk = remainingInk;
+  }
+
+  write(string){
+    
+  }
 }
 
 module.exports = Marker
--- src/room.js
+++ src/room.js
@@ -1,5 +1,9 @@
 class Room {
-  
+  constructor(name, description, contents){
+    this.name = name;
+    this.description = description;
+    this.contents = [];
+  }
 }
 
 module.exports = Room
