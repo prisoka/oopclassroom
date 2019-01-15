8c9221c4157d43320ed45f8f5ba85089bb7b9be3
Priscilla Okawa <prisoka@gmail.com>
2019-01-14 16:06:38 -0800

    class room OK

--- src/room.js
+++ src/room.js
@@ -4,6 +4,15 @@ class Room {
     this.description = description;
     this.contents = [];
   }
+
+  add(student){
+    this.contents.push(student)
+    return this
+  }
+
+  has(student){
+    return this.contents.includes(student)
+  }
 }
 
 module.exports = Room
